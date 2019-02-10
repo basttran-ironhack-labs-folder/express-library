888732aa545c3359f7ec11d8496e9b86b913485d
Bastien Tran <bastien.tran@gmail.com>
2019-02-10 03:16:28 +0100

    lesson example

--- routes/book-router.js
+++ routes/book-router.js
@@ -52,5 +52,67 @@ router.post("/process-book", (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.get("/book/:bookId/edit", (req, res, next) => {
+  // get the ID from the address (it's inside of req.params)
+  const { bookId } = req.params;
+
+  // find the book in the DB using the ID from the address
+  Book.findById(bookId)
+    .then(bookDoc => {
+      // send the database query result to the HBS file as "bookItem"
+      res.locals.bookItem = bookDoc;
+      res.render("book-edit.hbs");
+    })
+    // next(err) skips to the error handler in "bin/www" (error.hbs)
+    .catch(err => next(err));
+});
+
+router.post("/book/:bookId/process-edit", (req, res, next) => {
+  // res.json(req.body);
+  const { bookId } = req.params;
+  const { title, rating, description, author } = req.body;
+
+  Book.findByIdAndUpdate(
+    bookId, // ID of the document we want to update
+    { $set: { title, rating, description, author } }, // changes to make to that document
+    { runValidators: true } // additional settings (enforce the rules)
+  )
+    .then(bookDoc => {
+      // ALWAYS redirect if it's successful to avoid DUPLICATE DATE on refresh
+      // redirect ONLY to ADDRESSES - not HBS files
+      res.redirect(`/book/${bookDoc._id}`);
+    })
+    // next(err) skips to the error handler in "bin/www" (error.hbs)
+    .catch(err => next(err));
+});
+
+router.get("/book/:bookId/delete", (req, res, next) => {
+  // res.json(req.body);
+  const { bookId } = req.params;
+
+  Book.findByIdAndRemove(bookId)
+    .then(bookDoc => {
+      res.redirect("/books");
+    })
+    // next(err) skips to the error handler in "bin/www" (error.hbs)
+    .catch(err => next(err));
+});
+
+router.post("/book/:bookId/process-review", (req, res, next) => {
+  const { bookId } = req.params;
+  const { userFullName, reviewText } = req.body;
+
+  Book.findByIdAndUpdate(
+    bookId, // ID of the document we want to update
+    { $push: { reviews: { userFullName, reviewText } } }, // changes to the document
+    { runValidators: true } // additional settings (enforce the rules)
+  )
+    .then(bookDoc => {
+      res.redirect(`/book/${bookDoc._id}`);
+    })
+    // next(err) skips to the error handler in "bin/www"
+    .catch(err => next(err));
+});
+
 // share the router object with all the routes
 module.exports = router;
